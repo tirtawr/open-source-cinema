f82f76b41685bc17b6748a9520d59035c22de970
Tirta Rachman <tirtawening@gmail.com>
2021-03-12 15:01:42 -0500

    Add people of yesterday

---
+++ people-of-yesterday/sketch.js
@@ -0,0 +1,86 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+let video;
+let poseNet;
+let poses = [];
+
+function setup() {
+  createCanvas(1920, 1080);
+  video1 = createVideo(['video-1-cut.mp4']);
+  video1.size(width, height);
+
+  video2 = createVideo(['video-2-cut.mp4']);
+  video2.size(width, height);
+
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video1, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on("pose", function (results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video1.hide();
+  video2.hide();
+}
+
+function modelReady() {
+  select("#status").html("Click to start");
+}
+
+function draw() {
+  image(video2, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
+}
+
+function mousePressed() {
+  select("#status").html("");
+  video1.play();
+  video2.play();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i += 1) {
+    // For each pose detected, loop through all the keypoints
+    const pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j += 1) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      const keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i += 1) {
+    const skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j += 1) {
+      const partA = skeleton[j][0];
+      const partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
