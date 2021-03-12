49a67b496cfee0a37ca34d114ab8df5f22156208
Tirta Rachman <tirtawening@gmail.com>
2021-03-12 15:29:45 -0500

    Basic setup for dimensional-tones

---
+++ dimensional-tones/src/App.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-no-target-blank */
+import { useEffect } from 'react';
+
+import './App.css';
+import engine from './lib/Engine'
+
+function App() {
+
+  useEffect(() => {
+    console.log('asd')
+    engine.init()
+  });
+
+  return (
+    <div id="info">
+      <a href="https://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - drag controls<br />
+			Use "Shift+Click" to add/remove objects to/from a group.<br />
+			Grouped objects can be transformed as a union.
+    </div>
+  );
+}
+
+export default App;
